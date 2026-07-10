23fca32a017f09aaf22f8af192d8ac0caad740ba
Sergiy Shcherbyna <sergey.scherbina@gmail.com>
2026-07-11 00:34:19 +0200

    feat(v2.1): recognize symbolic extension operators

--- tests/e2e/v21-native-entry-smoke.sh
+++ tests/e2e/v21-native-entry-smoke.sh
@@ -70,6 +70,7 @@ constructor_guard_expected=$'enough\nlow\nmissing'
 [[ $(run_native "$FIXTURES/constructor-match-guard.ssc") == "$constructor_guard_expected" ]]
 [[ $(run_native "$FIXTURES/extension-declaration.ssc") == 'extension-header-ok' ]]
 [[ $(run_native "$FIXTURES/list-append.ssc") == '1,2,3,4' ]]
+[[ $(run_native "$FIXTURES/symbolic-extension-operators.ssc") == 'symbolic-operators-ok' ]]
 ui_fetch_json_expected=$'body:{"name":"Acme \\"HQ\\"","n":5}\nfetch-json:ok'
 [[ $(run_native "$ROOT/examples/ui-fetch-json.ssc") == "$ui_fetch_json_expected" ]]
 index_expected=$'ScalaScript 0.1 is running!\nSquares: 1, 4, 9, 16, 25'
@@ -87,6 +88,7 @@ index_expected=$'ScalaScript 0.1 is running!\nSquares: 1, 4, 9, 16, 25'
 [[ $(run_native --bytecode "$FIXTURES/constructor-match-guard.ssc") == "$constructor_guard_expected" ]]
 [[ $(run_native --bytecode "$FIXTURES/extension-declaration.ssc") == 'extension-header-ok' ]]
 [[ $(run_native --bytecode "$FIXTURES/list-append.ssc") == '1,2,3,4' ]]
+[[ $(run_native --bytecode "$FIXTURES/symbolic-extension-operators.ssc") == 'symbolic-operators-ok' ]]
 [[ $(run_native --bytecode "$ROOT/examples/ui-fetch-json.ssc") == "$ui_fetch_json_expected" ]]
 [[ $(run_native --bytecode "$ROOT/examples/index.ssc") == "$index_expected" ]]
 [[ $(run_native --bytecode "$FIXTURES/fs-os-provider.ssc") == "$fs_os_expected" ]]
